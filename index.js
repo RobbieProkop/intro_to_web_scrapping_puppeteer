02fdbaad90cdcba8fbbd60a6583dc425ca753517
Robbie Prokop <robbieprokop@gmail.com>
2022-11-14 23:03:30 +0400

    begin working on Run func

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+const puppeteer = require("puppeteer");
+
+async function run() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.goto("https://www.traversymedia.com");
+
+  await browser.close();
+}
+
+run();
