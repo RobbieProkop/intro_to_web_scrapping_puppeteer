2743fde140c8a57715c0a5a7c0b0c21e7dee4edb
Robbie Prokop <robbieprokop@gmail.com>
2025-02-08 14:47:56 -0700

    example stuff

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const fs = require("fs");
 
 const puppeteer = require("puppeteer");
+// async function delay(time) {
+//   await ((resolve) => {
+//     setTimeout(resolve,time)
+//   })
+// }
+function delay(time) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, time);
+  });
+}
 
 async function run() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  await page.goto("https://www.traversymedia.com");
+  await page.goto("https://www.instagram.com/creative.hair.styling", {
+    waitUntil: "networkidle2",
+  });
 
-  // await page.screenshot({ path: "example.png", fullPage: true });
+  // clcik the x to close login modal
+  await page.click("._abn5");
+  await page.screenshot({ path: "example.png", fullPage: true });
   // await page.pdf({ path: "example.pdf", format: "A4" });
 
   //Get HTML content
@@ -31,17 +45,36 @@ async function run() {
   //   }))
   // );
 
-  const courses = await page.$$eval("#courses .card", (el) =>
-    el.map((e) => ({
-      title: e.querySelector(".card-body h3").innerText,
-      level: e.querySelector(".card-body .level").innerText,
-      url: e.querySelector(".card-footer a").href,
-      promo: e.querySelector(".card-footer .promo-code .promo").innerText,
-    }))
+  // const courses = await page.$$eval("#courses .card", (el) =>
+  //   el.map((e) => ({
+  //     title: e.querySelector(".card-body h3").innerText,
+  //     level: e.querySelector(".card-body .level").innerText,
+  //     url: e.querySelector(".card-footer a").href,
+  //     promo: e.querySelector(".card-footer .promo-code .promo").innerText,
+  //   }))
+  // );
+
+  //Pull info from Instagram
+  // const images = await page.$$eval(".x1iyjqo2 div div ._ac7v", (el) =>
+  // );
+  // const link = document.querySelector("._aabd");
+
+  await page.waitForSelector("._aabd");
+  const images = await page.evaluate(() =>
+    Array.from(
+      document.querySelector("._aabd", (e) => ({
+        url: `https://www.instagram.com${e.querySelector("a")}`.href,
+        url: e.innerText,
+      }))
+      // document.querySelectorAll(
+      //   ".x1i10hfl.xjbqb8w.x6umtig.x1b1mbwd.xaqea5y.xav7gou.x9f619.x1ypdohk.xt0psk2.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x16tdsg8.x1hl2dhg.xggy1nq.x1a2a7pz._a6hd"
+      // ).href
+    )
   );
 
+  console.log("images :>> ", images);
   // Save Data to JSON file
-  fs.writeFile("courses.json", JSON.stringify(courses), (err) => {
+  fs.writeFile("imgURL.json", JSON.stringify(images), (err) => {
     if (err) throw new err();
     console.log("File Saved ");
   });
